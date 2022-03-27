8eca9e39ed96e404a6b9cb60f46896fd515868c0
pevaldob <102187121+pevaldob@users.noreply.github.com>
2022-03-27 22:46:38 +0200

    more reporting options

--- fduplicate.py
+++ fduplicate.py
@@ -4,30 +4,36 @@
 import os
 import hashlib
 
-hash_dict = {}
+hash_dict = {}  # hash : [<itm_list>]
+hash_path = {}  # hash : [<itm_list>]
+counter   = {}  # hash : counter
+RAPORT = ''   # folder list + summary
 
 # folder to check:
 CHECK_PATH = r'c:\check_folder'
 # path to log results:
 LOG_PATH = r'c:\result.txt'
 
+
+# /*------------------------
 def lst_file(path='', step=0):
-    if step==0:
-        print('start')
     if path:
+        global RAPORT
         for filename in os.listdir(path):
             f = os.path.join(path, filename)
             if os.path.isfile(f):
-                print("{0}{1}".format(' '*step,f))
+                type("{0}{1}".format(' '*step,f))
+                RAPORT += "{0}{1}\n".format(' '*step,f)
                 hashcollector(f)
             else:
-                print('{0}\\'.format(' '*step))
+                RAPORT += "{0}\\\n".format(' '*step)
                 lst_file(f, step+1)
     else:
         print('podaj sciezke...')
 
 def hashcollector(fname):
     itm = hashlib.md5()
+    # /* for small files for now
     with open(fname, 'rb') as fp:
         fdata = fp.read()
         if fdata:
@@ -43,4 +49,5 @@ lst_file(CHECK_PATH)
 
 with open(LOG_PATH, 'w', encoding="utf-8") as resultf:
     for key in hash_dict.keys():
-        resultf.write("{0} -> {1}\n".format(key, str(hash_dict[key])))
+        if counter[key] > 1:
+            resultf.write("----\n{0}: {1}\n -> {2}\n".format(counter[key], key, '\n'.join(hash_dict[key])))
